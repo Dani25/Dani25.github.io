db3a42de80469fd13e4f1cabd7e5e32190e339e6
Daniela Onita <danielaonita25@gmail.com>
2018-03-16 19:37:39 +0200

    spatiu

--- X3D/orientare_xz.js
+++ X3D/orientare_xz.js
@@ -8,6 +8,6 @@ function on_device_orientation(e)
 	var viewpoint = document.getElementById("id_viewpoint");
 	var z = radius * Math.cos(e.alpha * Math.PI / 180);
 	var x = radius * Math.sin(e.alpha * Math.PI / 180);
-	viewpoint.setAttribute("position", x + "0" + z);
+	viewpoint.setAttribute("position", x + " 0 " + z);
 	viewpoint.setAttribute("orientation", "0 1 0 " + e.alpha * Math.PI / 180);
 }
