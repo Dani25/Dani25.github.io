5b34c9081c9cb35adeacbe5391fe137284068143
Daniela Onita <danielaonita25@gmail.com>
2018-03-23 17:49:00 +0200

    news

--- X3D/ball.js
+++ X3D/ball.js
@@ -1,8 +1,15 @@
 setInterval(deseneaza,30);
 var y = 0;
+var directie = 1;
+
 function deseneaza()
 {
 	var t = document.getElementById("id_transform");
 	t.setAttribute("translation", "0 " + y + " 0");
-	y++;
+	y += directie;
+	if (y > 10)
+		directie = -1;
+	else 
+		if(y < 0)
+			directie = 1;
 }
